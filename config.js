3405411a448c4f613a6b51f4a698858de33d4120
tienphuoc1234 <95084926+tienphuoc1234@users.noreply.github.com>
2021-11-27 09:01:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "hé lô ban Thịnh",
+    introTitle: 'Trịnh Cường Thịnh,
+    introDesc: "Trả lời câu hỏi đi nhá
+    `thoát ra ăn cức nha,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'tớ đẹp trai lắm đúng khong ',
+    btnYes: 'dủng ruii <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thấy mình đẹp trai <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'tôi thấy ban dep trai nên tôi nói ban đẹp trai <33333333',
+    mess: 'Mình biết mà 🥰. ',
+    messDesc: 'Mình đẹp trai hơn ban luôn đúng khong.',
+    btnAccept: 'đúng rồi <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
