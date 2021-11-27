a8d172801f636a57cbeb502c58cf4cfdbeb3148c
tienphuoc1234 <95084926+tienphuoc1234@users.noreply.github.com>
2021-11-27 07:48:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'khi nào tui với ban đi chơi nhá.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <3.',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
